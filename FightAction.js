3251c10e41b39b4ae899f1632e2149c7799c7a9a
k-b-yamamoto <102796643+k-b-yamamoto@users.noreply.github.com>
2022-07-28 13:22:53 +0900

    モンスターをオブジェクト化

---
+++ FightAction.js
@@ -0,0 +1,169 @@
+'use strict';
+
+let d = 0;                                                  //戦闘ダメージ要素
+
+//ダメージ量算出式
+function GetDamage(a){
+  return( Math.floor( a * ( 1 + Math.random())));    //攻撃力の１〜２倍
+}
+
+//勇者の攻撃処理
+function HeroAttack(){
+  d = GetDamage( hero1.getLv + 3) - EnemyNumber;                     //ダメージ計算取得結果
+  Sword(hero1.getGSword);
+  d += swordAttack;
+  //console.log('ダメージ = ' + d);
+  //console.log('装備攻撃力' + swordAttack + 'が加算されました');
+  if(stuckNumber == 4){
+    stuck = true;
+  }    
+  //console.log('装備攻撃力' + swordAttack + 'が加算されました');  
+  if( d <= 0){
+    setMessage('ミス！', '攻撃が効かない');
+  } else if(IsBossClass){
+    setMessage('勇者の攻撃！', gFileBossClass[BossClassNumber].name + ' に ' + d + ' のダメージ！');
+    if(Math.floor(Math.random() * 101) > gFileBossClass[BossClassNumber].mHp){
+      gMessage2 = `しかし${gFileBossClass[BossClassNumber].name}は攻撃をかわした！`;
+      d = 0;
+      // console.log('ダメージ = ' + d);
+    }
+  } else {
+    setMessage('勇者の攻撃！', monster.getEnemyName + 'に ' + d + ' のダメージ！');
+    if(Math.floor(Math.random() * (10 - EnemyNumber)) > 5){
+      gMessage2 = `しかし${monster.getEnemyName}は攻撃をかわした！`;
+      d = 0;
+    }
+  }
+  if (kaishin == 1){
+    gMessage1 = '会心の一撃！';
+  }
+  if (miracle == 7){
+    gMessage1 = '奇跡の一撃!!';
+  }
+  if (magicAttack == 1){
+    gMessage1 = '命削りの一撃!!';
+    recoil(hero1.getHp);
+    hero1.setHp = recoilDamage;
+  }
+  return d;
+}
+
+function MonsterAttack(){
+  let d = GetDamage( EnemyNumber + 2);
+  ShoesEffect(gShoes);
+  if(IsBossClass){
+    BossPower(BossClassNumber);
+    //console.log('dSP = ' + dSP);
+    let avoidJudge = Math.floor(Math.random() * (Math.floor(gFileBossClass[BossClassNumber].mHp / 5)));
+    //console.log("gavoid = " + gAvoid);
+    //console.log("avoidjudge = " + avoidJudge);
+    setMessage( gFileBossClass[BossClassNumber].name + ' の攻撃！', dSP + ' のダメージ！');
+    if(Math.floor(hero1.getGAvoid) > avoidJudge){
+      dSP = 0;
+      gMessage2 = 'しかし勇者はヒラリとかわした！';
+    }
+    hero1.setHp = hero1.getHp - dSP;
+  } else {
+    let avoidJudge = Math.floor(Math.random() * (EnemyNumber + 5));  
+    setMessage( monster.getEnemyName + 'の攻撃！', d + ' のダメージ！');
+    if(Math.floor(hero1.getGAvoid) > avoidJudge){
+      d = 0;
+      gMessage2 = 'しかし勇者はヒラリとかわした！';
+    }
+    hero1.setHp = hero1.getHp - d;
+    // return gHP;
+  }
+}
+
+function StuckEffect(gSword){
+  if(gSword == 2){
+    if(IsTrueBoss == 2){
+      setMessage( gFileBossClass[BossClassNumber].name + ' は', 'こちらの剣を悲しげに見つめている');          
+    } else if(IsBossClass){
+      setMessage( gFileBossClass[BossClassNumber].name + 'は', '剣の威力にひるんで動けない');          
+    } else {
+      setMessage( monster.getEnemyName + 'は', '剣の威力にひるんで動けない！');          
+    }  
+  } else if(gSword == 3){
+    if(IsBossClass){
+      setMessage( gFileBossClass[BossClassNumber].name + 'は', '魔剣の魔力で動けない！');          
+    } else {
+      setMessage( monster.getEnemyName + 'は', '魔剣の魔力で動けない！');          
+    }
+  }
+  return gPhase;
+}
+
+// ゲームオーバー時復帰処理
+function GameOver(){
+  switch(gCursor){
+    case 0:                                          //「コンティニュー」を選択
+      if(IsBoss == 1){                               //ボスに負けた場合
+        IsBoss = 0;
+      }
+      if(IsMid_Boss == 1){                           //中ボスに負けた場合
+        IsMid_Boss = 0;
+      }
+      if(IsTrueBoss == 2){                           //裏ボスに負けた場合
+        IsTrueBoss = 4;
+      }
+      if(gGuard == 1){
+        gGuard -= 1;
+        gItem = 0;
+      }
+      if(gGuard == 3){
+        gGuard -= 1;
+      }
+      LoadData();
+      gAngle = 0;
+      BossClassNumber = null;
+    break;
+
+    case 1:                                         //「はじめから」を選択
+      hero1.setHp = StartStatus[0];
+      hero1.setMhp = StartStatus[1];
+      hero1.setGEx = StartStatus[2];
+      hero1.setLv = StartStatus[3];
+      hero1.setEnforce = StartStatus[4];
+      hero1.setGItem = StartStatus[5];
+      hero1.setSpeed = StartStatus[6];
+      hero1.setGSword = StartStatus[7];
+      hero1.setPlayerX = StartStatus[8] * TILESIZE + TILESIZE / 2;
+      hero1.setPlayerY = StartStatus[9] * TILESIZE + TILESIZE / 2;
+      hero1.setGAvoid = StartStatus[10];
+      gAngle = 0;
+      gCursor = 0;                                    
+      IsBoss = 0;
+      IsMid_Boss = 0;
+      IsTrueBoss = 0;
+      BossClassNumber = null;
+    break;
+  }
+  return;
+}
+
+function Run(){
+  if(IsBossClass){
+    setMessage('逃げることができない！', null);
+    gPhase = 5;
+    gCursor = 0;
+    gSirge = 1;
+  } else if(Math.floor(Math.random() * (10 * hero1.getSpeed)) > 2){                         //「逃げる」成功
+    setMessage('なんとか逃げ切れた', null);
+    //console.log('逃走経験値 = ' + (gEnemyMHP - gEnemyHP));
+    gPhase = 8;
+  } else {                                               //「逃げる」失敗
+    setMessage('回り込まれてしまった', null);
+    gPhase = 5;
+    gCursor = 0;
+    gSirge = 1;
+  }
+  return gPhase;
+}
+
+
+// ゲームオーバー時メッセージ
+function setGOMessage(){
+  setMessage('    コンティニュー', '    はじめから');
+  gMessage3 = 'Game Over';  
+}
---
+++ FightOperator.js
@@ -0,0 +1,121 @@
+'use strict';
+
+let gSirge = 0;                                             //戦闘時包囲要素
+let OrderDice;                                              //戦闘順序決定要素
+//モンスター登場時にmonsterインスタンスを生成するように変更したい。
+const monster = new Monster();                              //モンスターインスタンス
+
+function CommandFight(){
+  gPhase = 2;                                        //戦闘コマンド選択フェーズ
+  setMessage('　戦う', '　逃げる');
+  stuck = false;
+  if(IsBossClass){
+    setBossNumber(IsBoss, IsMid_Boss, IsTrueBoss, gGuard);
+    setBossHP(BossClassNumber);
+  } else {
+    setMonsterNumber();
+    console.log('EnemyNumber = ' + EnemyNumber);
+    monster.setEnemyName = EnemyNumber;
+    monster.setEnemyMHp = EnemyNumber;
+    monster.setEnemyHp = monster.getEnemyMHP;
+    console.log('モンスターインスタンスをsetしました');
+    // const monster = new Monster(EnemyNumber);
+    // setEnemyHp(EnemyNumber);
+    //console.log('gEnemyMHP = ' + gEnemyMHP);
+    //console.log('gEnemyHP = ' + gEnemyHP);
+  }
+}
+
+function CommandFightII(){
+  gPhase = 3;                                        //戦闘コマンド選択フェーズ
+  setMessage('　戦う', '　逃げる');
+  //console.log('敵の残存HP = ' + gEnemyHP);
+  //console.log('ボスの残存HP = ' + gBossHP);
+}
+
+function judgeOrder(gShoes){                         //戦闘行動順決定関数
+  ShoesEffect(gShoes);
+  // console.log('gplusspeed = ' + gPlusSpeed);
+  OrderDice = Math.floor(Math.random() * (2 + Math.round(hero1.getSpeed + gPlusSpeed)));   //戦闘行動順
+  // console.log('orderDice = ' + OrderDice);
+  return OrderDice;
+}
+
+
+//戦闘行動処理関数
+function operateFight(gOrder){
+  BossEvent();
+  setBossNumber(IsBoss, IsMid_Boss, IsTrueBoss, gGuard);
+  if(IsTrueBoss == 2){
+    EnemyNumber = 10;
+  }
+  if(gCursor == 0){                                                 //「戦う」を選択
+    if (gPhase == 2 || gPhase == 3){                                //戦闘行動順を反映
+      if(gOrder == 0){
+        gPhase = 5;
+      } else {
+        gPhase = 4;
+      }
+    }
+
+    if (gPhase == 4){
+      //console.log('現在のターンは ' + gPhase);
+      HeroAttack();                                                 //勇者の攻撃処理
+      if(IsBossClass){                                               //ボスクラスダメージ処理
+        gBossHP -= Math.max(d, -1);
+        if(gBossHP <= 0){
+          gPhase = 7;
+          return;
+        }
+      } else {                                                      //敵ダメージ処理
+        console.log('ダメージ = ' + d);
+        monster.setEnemyHp = monster.getEnemyHP - Math.max(d, -1);
+        console.log('ターン4終了時の敵HP = ' + monster.getEnemyHP);
+        if(monster.getEnemyHP <= 0){
+          gPhase = 7;
+          return;
+        }
+      }
+      // console.log('gOrder = ' + gOrder);
+      if(gOrder == 0){
+        gPhase = 6;
+        //console.log('ループを脱します');
+        return;
+      } else {
+        gPhase = 5;
+        return;
+      }
+    }
+
+    if (gPhase == 5){
+      //console.log('ターン5開始時の敵HP = ' + gEnemyHP);
+      if(stuck == true){
+        StuckEffect(gSword);
+        stuck = false;
+      } else {
+        MonsterAttack();
+      }
+      // console.log('戦闘ループ処理を開始します。');
+      if (hero1.getHp <= 0){                                               //プレイヤーが死亡した場合
+        hero1.setHp = 0;
+        gPhase = 10;                                                //死亡フェイズ
+      } else if(gOrder > 0 || gSirge == 1) {                        //自分が先制攻撃した場合と逃げるコマンドで回り込まれた場合
+        // console.log('gSirge = ' + gSirge);
+        gSirge = 0;
+        gPhase = 6;                                                 //戦闘コマンドへループ
+      } else {
+        // console.log('gSirge = ' + gSirge);
+        // console.log('gOrder = ' + gOrder);
+        gPhase = 4;
+      }
+      return;
+    }
+  } else if(gCursor == 1){                                          //「逃げる」コマンド選択時
+    ShoesEffect(gShoes);
+    hero1.setSpeed = hero1.getSpeed + gPlusSpeed;
+    Run();
+    return;
+  }
+  gPhase ++;                                             //フェーズ経過
+  return;
+}
